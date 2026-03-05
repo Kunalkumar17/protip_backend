4bcab1d7ea6329ed77b3070c74937bcd4e0bbd79
kunal kumar <kunalkumar17aug@gmail.com>
2026-03-05 09:05:12 +0530

    update websocket.js

--- websocket.js
+++ websocket.js
@@ -1,6 +1,7 @@
 import { WebSocketServer } from "ws";
 
 let clients = [];
+let currentGoalTotal = 0;
 
 export const initWebSocket = (server) => {
   const wss = new WebSocketServer({ server });
@@ -10,16 +11,45 @@ export const initWebSocket = (server) => {
 
     clients.push(ws);
 
+    // Send current goal to new overlays
+    ws.send(JSON.stringify({
+      type: "goalInit",
+      total: currentGoalTotal
+    }));
+
     ws.on("close", () => {
       clients = clients.filter(c => c !== ws);
+      console.log("WS client disconnected");
     });
   });
 };
 
 export const broadcastTip = (tip) => {
+
+  const amount = Number(tip.amount) || 0;
+
+  currentGoalTotal += amount;
+
+  const alertMessage = {
+    type: "tipAlert",
+    name: tip.name || "Anonymous",
+    amount: amount,
+    message: tip.message || ""
+  };
+
+  const goalMessage = {
+    type: "goalUpdate",
+    total: currentGoalTotal
+  };
+
   clients.forEach(client => {
-    if (client.readyState === 1) {
-      client.send(JSON.stringify(tip));
+    if (client.readyState === client.OPEN) {
+
+      // Send tip alert
+      client.send(JSON.stringify(alertMessage));
+
+      // Send goal update
+      client.send(JSON.stringify(goalMessage));
     }
   });
 };
